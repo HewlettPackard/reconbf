54e104360dd21bcb019cdc2460e9d14bc9bb1dbf
Grant Murphy <grant.murphy@hp.com>
2015-04-17 09:06:07 -0700

    Ensure proper controls of /proc/$pid/maps
    Add some tests to ensure that proper controls are in place to prevent processes
    reading the memory space of other processes unless they are running as the same
    user.
    
    Change-Id: I32f5942522bf25d2843946d33630a09a09eb62e3

--- modules/test_kernel.py
+++ modules/test_kernel.py
@@ -1,5 +1,10 @@
+
+import ctypes
+import errno
 import os
+import pwd
 import subprocess
+from multiprocessing import Process, Value
 
 from lib import test_class
 from lib.test_result import GroupTestResult, Result, TestResult
@@ -137,5 +142,105 @@ def test_pax():
             results.add_result(test, TestResult(Result.PASS))
         else:
             results.add_result(test, TestResult(Result.FAIL))
+
+    return results
+
+
+def _maps_readable(pid):
+    files = [
+        '/proc/{}/maps'.format(pid),
+        '/proc/{}/smaps'.format(pid),
+        '/proc/{}/task/{}/maps'.format(pid, pid),
+        '/proc/{}/task/{}/smaps'.format(pid, pid),
+    ]
+
+    for filename in files:
+        try:
+            with open(filename) as f:
+                return f.read()
+        except IOError as e:
+            if e.errno not in [errno.EPERM, errno.EACCES]:
+                raise e
+
+
+def _can_read_from_own(result):
+    """Make sure we can read our own /proc/$pid/maps"""
+    pid = os.getpid()
+    result.value = True if _maps_readable(pid) else False
+
+
+def _can_read_any_with_same_uid(result):
+    """Make sure we can read /proc/$pid maps for any
+    process owned by us. In this case we use parent
+    process that spawned this process (using multiprocessing).
+    """
+    parent_pid = os.getppid()
+    result.value = True if _maps_readable(parent_pid) else False
+
+
+def _cant_read_others(result):
+    """Make sure that we can't read any other users maps file. To
+    do this we need to switch to a less privileged user
+    account as rbf.py runs as root.
+    """
+    # Change to nobody user
+    os.setuid(pwd.getpwnam('nobody').pw_uid)
+
+    # Make sure we can't read init process (owned by root)
+    result.value = True if _maps_readable(1) else False
+
+
+def _cant_read_parents_when_priv_dropped(result):
+    """Make sure that we can't read our parents maps file
+    when privileges are dropped."""
+
+    # Change to nobody user
+    os.setuid(pwd.getpwnam('nobody').pw_uid)
+
+    # Make sure can't read parents process (owned by root)
+    result.value = True if _maps_readable(os.getppid()) else False
+
+
+@test_class.explanation("""
+    Protection name: /proc/$pid/maps
+
+    Check: Ensures that the correct access controls are in place for
+    process memory layouts.
+
+    Purpose:
+        * Ensure that a /proc maps aren't world readable
+        * Ensure that setuid priv-dropped process can still read
+          their own maps files.
+    """)
+def test_proc_map_access():
+    tests = {
+        "Can read own /proc/$pid/maps file": {
+            "function": _can_read_from_own,
+            "expected": True
+        },
+        "Can read others process /proc/$pid/maps with same UID": {
+            "function": _can_read_any_with_same_uid,
+            "expected": True
+        },
+        "Can't read /proc/$pid/maps of other processes": {
+            "function": _cant_read_others,
+            "expected": False
+        },
+        "Can't read parents after privileges were dropped": {
+            "function": _cant_read_parents_when_priv_dropped,
+            "expected": False
+        }
+    }
+    results = GroupTestResult()
+    for t in tests:
+        fn = tests[t]["function"]
+        exp = tests[t]["expected"]
+        act = Value(ctypes.c_bool)
+        p = Process(target=fn, args=(act,))
+        p.start()
+        p.join()
+
+        result = Result.PASS if exp == act.value else Result.FAIL
+        results.add_result(t, TestResult(result))
 
     return results
