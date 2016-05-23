4a17f6aa8b27a69667a6974eb595c1213ddfa2f3
Stanisław Pitucha <stanislaw.pitucha@hp.com>
2016-05-23 13:03:21 +1000

    [firewall] Ensure whitelisting
    Make sure the iptables is set up to drop unknown packets by default, rather than
    using blacklisting only.
    
    Change-Id: I34757fb9cf9feb3afb7cba43196c3b4454a2ff67

---
+++ reconbf/modules/test_firewall.py
@@ -0,0 +1,74 @@
+from reconbf.lib import test_class
+from reconbf.lib.result import Result
+from reconbf.lib.result import TestResult
+
+import collections
+import subprocess
+
+
+def _list_rules():
+    try:
+        output = subprocess.check_output(['iptables-save'])
+    except (IOError, subprocess.CalledProcessError):
+        # cannot get the list of rules for some reason
+        return None
+
+    lines = [line.strip() for line in output.splitlines()
+             if not line.startswith(b'#')]
+    return lines
+
+
+def _get_default_policy(rules):
+    """Get the default policy for each table/chain."""
+    tables = collections.defaultdict(dict)
+    current_table = None
+
+    for rule in rules:
+        if rule.startswith(b'*'):
+            current_table = rule[1:]
+
+        if rule.startswith(b':'):
+            parts = rule[1:].split()
+            tables[current_table][parts[0]] = parts[1]
+
+    return tables
+
+
+@test_class.explanation("""
+    Protection name: Firewall whitelisting
+
+    Check: Make sure that the firewall is configured to reject
+    packets by default.
+
+    Purpose: Creating whitelists is usually more secure than
+    blacklists. Defaulting to dropping unknown traffic is a safer
+    option in case of missed rules.
+    """)
+def firewall_whitelisting():
+    rules = _list_rules()
+    if rules is None:
+        return TestResult(Result.SKIP, "Cannot retrieve iptables rules")
+
+    targets = _get_default_policy(rules)
+    if b'filter' not in targets:
+        return TestResult(Result.SKIP, "Cannot find the filter table")
+
+    failures = []
+
+    filter_table = targets[b'filter']
+    if b'INPUT' not in filter_table:
+        return TestResult(Result.SKIP, "Filter table doesn't include INPUT")
+    if b'FORWARD' not in filter_table:
+        return TestResult(Result.SKIP, "Filter table doesn't include FORWARD")
+
+    if filter_table[b'INPUT'] == b'ACCEPT':
+        failures.append('INPUT')
+    if filter_table[b'FORWARD'] == b'ACCEPT':
+        failures.append('FORWARD')
+
+    if failures:
+        return TestResult(Result.FAIL,
+                          "The following chains accept packets by "
+                          "default: %s" % ', '.join(failures))
+    else:
+        return TestResult(Result.PASS, "Filter chains whitelist by default")
